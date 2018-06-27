922fa93334874a5b4d541a3c0af42ccbbec67233
Vutivi <201141790@student.uj.ac.za>
2018-06-27 22:17:30 +0200

    Getting name of beer for foreign key in beer

--- beerapp/admin.py
+++ beerapp/admin.py
@@ -3,9 +3,14 @@ from django.contrib import admin
 # Register your models here.
 from .models import Beer, Review
 
-admin.site.register(Beer)
+#admin.site.register(Beer)
+class BeerAdmin(admin.ModelAdmin):
+	list_display = ('name','ibu', 'calories', 'abv', 'style', 'brewery_location')
+
+	
 #admin.site.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
 	list_display = ('beer','aroma', 'appearance', 'taste', 'overall')
 
 admin.site.register(Review, ReviewAdmin)
+admin.site.register(Beer, BeerAdmin)
