0d37dab3774579ffaf92b85d7ebc0b0647a8e1e2
Vutivi <201141790@student.uj.ac.za>
2018-06-27 20:29:39 +0200

    added landing page

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -4,13 +4,13 @@ from django.urls import path
 from django.contrib import admin
 admin.autodiscover()
 
-import hello.views
+import beerapp.views
 
 # Examples:
 # url(r'^$', 'gettingstarted.views.home', name='home'),
 # url(r'^blog/', include('blog.urls')),
 
 urlpatterns = [
-    url(r'^$', hello.views.index, name='index'),
+    url(r'^$', beerapp.views.index, name='index'),
     path('admin/', admin.site.urls),
 ]
