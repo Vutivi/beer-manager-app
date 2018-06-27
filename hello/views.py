5dcc0ecbc13a0bdd650869232224b74f8c821cc0
Vutivi <201141790@student.uj.ac.za>
2018-06-27 19:36:11 +0200

    heroku push

--- hello/views.py
+++ hello/views.py
@@ -2,9 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-from django.http import HttpResponse
-from .models import Beer, Review
-
-
 def index(request):
-	return HttpResponse("Hello, world. You're at the polls index.")
+    # return HttpResponse('Hello from Python!')
+    return render(request, 'index.html')
